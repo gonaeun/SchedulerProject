27f5097a1003a1bfbd6437cf9f24a9cfaf8f4d69
고나은 <gonaeunn@gmail.com>
2025-03-25 02:51:05 +0900

    feat: add repository implements

--- scheduleproject/src/main/java/com/schedule/scheduleproject/repository/ScheduleRepository.java
+++ scheduleproject/src/main/java/com/schedule/scheduleproject/repository/ScheduleRepository.java
@@ -4,12 +4,13 @@ import com.schedule.scheduleproject.entity.Schedule;
 import java.util.List;
 
 public interface ScheduleRepository {
+    // 이거 인터페이스니까 >> 이걸 implements ScheduleRepository한 클래스는 이 인터페이스의 모든 메서드를 꼭 구현해야 함!
 
     Schedule saveSchedule(Schedule schedule); // 일정 생성, 수정
 
     List<Schedule> findAllSchedules();  // 전체 일정 조회
 
-    Schedule findMemoById(Long id);  // 단건 일정 조회
+    Schedule findScheduleById(Long id);  // 단건 일정 조회
 
     void deleteSchedule(Long id); // 일정 삭제
 }
