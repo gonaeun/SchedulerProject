9b3101c8d8baa199bf62c62ad032db0077efb54f
고나은 <gonaeunn@gmail.com>
2025-03-25 20:08:48 +0900

    feat: add Controller

---
+++ scheduleproject/src/main/java/com/schedule/scheduleproject/controller/ScheduleController.java
@@ -0,0 +1,49 @@
+package com.schedule.scheduleproject.controller;
+
+
+import com.schedule.scheduleproject.entity.Schedule;
+import com.schedule.scheduleproject.service.ScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController // @Controller + @ResponseBody
+@RequestMapping("/schedules")  // Prefix
+public class ScheduleController {
+
+    // 주입된 의존성을 변경할 수 없어서 객체의 상태를 안전하게 유지할 수 있음
+    private final ScheduleService scheduleService;
+
+    // 생성자 주입 : 클래스가 필요로 하는 의존성을 생성자를 통해 전달하는 방식
+    // 그 대상은 ScheduleService 의 구현체인 ScheduleServiceImpl
+    @Autowired
+    public ScheduleController(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
+    }
+
+    // 일정 생성
+    @PostMapping
+    public Schedule createSchedule(@RequestBody Schedule schedule) {
+        return scheduleService.saveSchedule(schedule);
+    }
+
+    // 전체 일정 조회
+    @GetMapping
+    public List<Schedule> getAllSchedules() {
+        return scheduleService.getAllSchedules();
+    }
+
+    // 단건 일정 조회
+    @GetMapping("/{id}")
+    public Schedule getSchedule(@PathVariable long id) {
+        return scheduleService.getScheduleById(id);
+    }
+
+    // 일정 삭제
+    @DeleteMapping("/{id}")
+    public void deleteSchedule(@PathVariable long id) {
+        scheduleService.deleteSchedule(id);
+    }
+
+}
